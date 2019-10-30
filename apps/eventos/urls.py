5befe842c20c52b56dd799c944f195a7d02b5f09
gaboroa14 <gaboroab14@gmail.com>
2019-10-30 13:41:20 -0400

    work in progress

---
+++ apps/eventos/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='inicio.html'),name="home"),
+]
