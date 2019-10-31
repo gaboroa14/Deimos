6ff78657672fddaca7b4fce2f4430e322ad75436
gaboroa14 <gaboroab14@gmail.com>
2019-10-31 00:28:55 -0400

    hasta aqui quede

--- apps/eventos/urls.py
+++ apps/eventos/urls.py
@@ -4,4 +4,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='inicio.html'),name="home"),
+    path('usuarios/', TemplateView.as_view(template_name='usuarios.html'),name="user")
 ]
